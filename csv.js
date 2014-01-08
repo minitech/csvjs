3a7e7946f80655a735e600bcf0e0c15a7f49e2c9
Ryan O’Hara <rninty@gmail.com>
2014-01-08 08:15:27 -0800

    Fixed consecutive commas.
    Oops.

--- csv.js
+++ csv.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function parse(csv) {
-	var PART = /"((?:""|[^"])*)"(?=,|\r\n|$)|,|\r\n|(?!$)((?!\r\n)[^,])*/g;
+	var PART = /"((?:""|[^"])*)"(?=,|\r\n|$)|\r\n|(?!$)(((?!\r\n)[^,])*)(?:,|$|(?=\r\n))/g;
 	var match;
 
 	var rows = [];
@@ -10,12 +10,10 @@ function parse(csv) {
 	while (match = PART.exec(csv)) {
 		if (match[1] === undefined) {
 			if (match[2] === undefined) {
-				if (match[0] === '\r\n') {
-					rows.push(current);
-					current = [];
-				}
+				rows.push(current);
+				current = [];
 			} else {
-				current.push(match[0]);
+				current.push(match[2]);
 			}
 		} else {
 			current.push(match[1].replace(/""/g, '"'));
