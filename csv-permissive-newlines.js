2d4d7f37c10aee26a69ad0bd80c23b862855f5b5
Ryan O’Hara <rninty@gmail.com>
2014-01-08 09:54:56 -0800

    Added version that accepts all line endings.

---
+++ csv-permissive-newlines.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function parse(csv) {
+	var PART = /"((?:""|[^"])*)"(?=[,\r\n]|$)|\r\n|[\r\n]|(?!$)(((?![\r\n])[^,])*)(?:,|$|(?=[\r\n]))/g;
+	var match;
+
+	var rows = [];
+	var current = [];
+
+	while (match = PART.exec(csv)) {
+		if (match[1] === undefined) {
+			if (match[2] === undefined) {
+				rows.push(current);
+				current = [];
+			} else {
+				current.push(match[2]);
+			}
+		} else {
+			current.push(match[1].replace(/""/g, '"'));
+		}
+	}
+
+	if (current.length) {
+		rows.push(current);
+	}
+
+	return rows;
+}
